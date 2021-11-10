1f990c79e50036d9c97065ea55683917ede697af
ignore87 <94009242+ignore87@users.noreply.github.com>
2021-11-10 19:43:41 +0200

    Create task_1_2.py

---
+++ task_1_2.py
@@ -0,0 +1,7 @@
+x = float(input("Введите перове число:"))
+y = float(input("Введите второе число:"))
+sqrt = (abs(x) - abs(y))/(1 + abs(x*y))
+print("Ответ:")
+print(sqrt)
+
+exit = print(input("Нажмите Enter для выхода"))
