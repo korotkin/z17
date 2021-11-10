a53bd78a71f10353b1f234b72861bebeb5d966c2
ignore87 <94009242+ignore87@users.noreply.github.com>
2021-11-10 19:44:47 +0200

    Create task_1_4.py

---
+++ task_1_4.py
@@ -0,0 +1,12 @@
+a = float(input("Введите перове число:"))
+b = float(input("Введите второе число:"))
+c = (a + b)/2
+print("Среднее арифметическое:")
+print(c)
+import math
+a = float(input("Введите перове число:"))
+b = float(input("Введите второе число:"))
+sqrt = math.sqrt(a*b)
+print("Среднее геометрическое:")
+print(sqrt)
+exit = print(input("Нажмите Enter для выхода"))
