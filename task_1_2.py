7b9b8b9a47874a62d5c467cde36cf15de3ab0868
Nikita Degtyarev <korbofos.nn@gmail.com>
2021-11-11 19:56:16 +0300

    nw01

---
+++ task_1_2.py
@@ -0,0 +1,5 @@
+x = 10
+y = -15
+
+result = (abs(x) - abs(y)) / (1 + abs(x * y))
+print(result)
---
+++ task_1_3.py
@@ -0,0 +1,7 @@
+length = 5
+
+
+volume = length ** 3
+square = (length ** 2) * 4
+
+print("Площадь равна: ", volume, "\nПериметр равен: ", square)
