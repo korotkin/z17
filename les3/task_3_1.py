c62c5ffe2a2802bfa4e105c9f481047490733299
NickNaPozitive <51098504+NickNaPozitive@users.noreply.github.com>
2021-11-12 02:33:10 +0300

    Delete les3 directory

--- les3/task_3_1.py
+++
@@ -1,4 +0,0 @@
-data_in = int(input("Введите число: "))
-
-if data_in % 1000 == 0:
-    print("millennium")
--- les3/task_3_2.py
+++
@@ -1,8 +0,0 @@
-data_in = int(input("Введите возраст: "))
-
-if data_in > 50:
-    print("ресторан")
-elif 20 < data_in < 50:
-    print("кафе")
-else:
-    print("дом")
