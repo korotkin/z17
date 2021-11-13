19c4d9d321fb4fc24cd32335dc930e1fc49ba643
Anton Jegorov <94009242+ignore87@users.noreply.github.com>
2021-11-13 19:28:43 +0200

    Create task_6_13.py

---
+++ task_6_13.py
@@ -0,0 +1,26 @@
+from random import randint
+print("Разница двух матриц:")
+n = 5
+m = 4
+matrix_a = []
+for i in range(m):
+    h=[]
+    for j in range(n):
+        h.append(randint(1, 66))
+    matrix_a.append(h)
+n = 5
+m = 4
+matrix_b = []
+for i in range(m):
+    h = []
+    for j in range(n):
+        h.append(randint(1, 66))
+    matrix_b.append(h)
+answer = []
+for i in range(m):
+    z = []
+    for j in range(n):
+        z.append(matrix_a[i][j]-matrix_b[i][j])
+    answer.append(z)
+for i in answer:
+    print(i)
